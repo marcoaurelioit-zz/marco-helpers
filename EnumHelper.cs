bcec8256951f9ba42f6f446e40bc9422b85e79bb
Marco Aurélio <maureliobrasil@gmail.com>
2018-11-13 12:51:22 -0200

    Update EnumHelper.cs

--- EnumHelper.cs
+++ EnumHelper.cs
@@ -6,34 +6,33 @@ namespace Marco.Helpers
 {
     public static class EnumHelper
     {
-        public static string GetDisplayName<T>(object value) where T : struct, IConvertible
+        public static string GetDisplayName<T>(int value) where T : struct, IConvertible
         {
-            var result = TryParseEnum<T>(value);
+            var result = TryParseEnumOrNull<T>(value);
             var attr = GetDisplayAttribute(result);
             return attr != null ? attr.Name : null;
         }
 
-        public static string GetDescription<T>(object value) where T : struct, IConvertible
+        public static string GetDescription<T>(int value) where T : struct, IConvertible
         {
-            var result = TryParseEnum<T>(value);
+            var result = TryParseEnumOrNull<T>(value);
             var attr = GetDisplayAttribute(result);
             return attr != null ? attr.Description : null;
-        }
-
-        public static T TryParseEnum<T>(object value) where T : struct, IConvertible
+        }     
+        public static T TryParseEnumOrDefault<T>(int intValue) where T : struct, IConvertible
         {
             var type = typeof(T);
 
             if (!type.IsEnum)
                 throw new ArgumentException("O tipo informado não é um enumerador.");
 
-            if (Enum.IsDefined(type, value))
-                return (T)Enum.Parse(type, value.ToString(), true);
+            if (Enum.IsDefined(type, intValue))
+                return (T)Enum.ToObject(type, intValue);
 
             return default(T);
         }
 
-        public static T? TryParseEnumOrNull<T>(object value) where T : struct, IConvertible
+        public static T? TryParseEnumOrNull<T>(int value) where T : struct, IConvertible
         {
             var type = typeof(T);
 
@@ -41,19 +40,19 @@ namespace Marco.Helpers
                 throw new ArgumentException("O tipo informado não é um enumerador.");
 
             if (Enum.IsDefined(type, value))
-                return (T)Enum.Parse(type, value.ToString(), true);
+                return (T)Enum.ToObject(type, value);
 
-            return default(T?);
+            return null;
         }
 
         private static DisplayAttribute GetDisplayAttribute(object value)
         {
-            if (value is null)
-                throw new ArgumentNullException(nameof(value));
+            if (value is null) return null;
 
             var type = value.GetType();
             var field = type.GetField(value.ToString());
-            return field == null ? null : field.GetCustomAttribute<DisplayAttribute>();
+
+            return field != null ? field.GetCustomAttribute<DisplayAttribute>() : null;
         }
     }
 }
